205614684efce030d85e15c8226da160c640b05a
Kor de Jong <k.dejong@geoneric.eu>
2014-10-27 21:53:10 +0100

    Added support for Boost 1.55.0

---
+++ cmake/package/boost/configure-1.55.0.cmake
@@ -0,0 +1,12 @@
+if(WIN32 AND
+        (${compiler_id} STREQUAL "mingw") AND
+        (${target_architecture} STREQUAL "x86_64"))
+    find_program(ml64 ml64)
+
+    if(NOT ml64)
+        MESSAGE(SEND_ERROR
+            "peacock: ml64 is required for building Boost.Context,\n"
+            "peacock: but it could not be found\n"
+            "peacock: Add its location to PATH")
+    endif()
+endif()
