b07934cc1541b17f518a1d0e1d51b90ef59b6638
Kor de Jong <k.dejong@geoneric.eu>
2015-04-02 14:27:20 +0200

    Initialize variables

--- cmake/package/boost/configure.cmake
+++ cmake/package/boost/configure.cmake
@@ -3,11 +3,20 @@ set(boost_version "1.57.0")
 
 if(build_boost)
     set(boost_version ${boost_version} CACHE STRING "Version of Boost to build")
+    set(boost_build_boost_atomic FALSE CACHE BOOL "Build Atomic library")
+    set(boost_build_boost_chrono FALSE CACHE BOOL "Build Chrono library")
+    set(boost_build_boost_date_time FALSE CACHE BOOL "Build DateTime library")
     set(boost_build_boost_filesystem FALSE CACHE BOOL
         "Build Filesystem library")
+    set(boost_build_boost_program_options FALSE CACHE BOOL
+        "Build Program Options library")
     set(boost_build_boost_python FALSE CACHE BOOL "Build Python library")
+    set(boost_build_boost_regex FALSE CACHE BOOL "Build Regex library")
+    set(boost_build_boost_serialization FALSE CACHE BOOL
+        "Build Serialization library")
     set(boost_build_boost_system FALSE CACHE BOOL "Build System library")
     set(boost_build_boost_test FALSE CACHE BOOL "Build Test library")
+    set(boost_build_boost_thread FALSE CACHE BOOL "Build Thread library")
     set(boost_build_boost_timer FALSE CACHE BOOL "Build Timer library")
 
     # http://www.boost.org/doc/libs/1_57_0/more/getting_started/unix-variants.html#header-only-libraries
